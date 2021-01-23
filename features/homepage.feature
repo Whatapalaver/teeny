96482e2442059f148d5401689e716d7463999ae1
Angela Wolff <warriorwomenblog@gmail.com>
2021-01-23 16:21:05 +0000

    Added failing test for Add New Url

--- features/homepage.feature
+++ features/homepage.feature
@@ -3,3 +3,14 @@ Feature: Homepage
   Scenario: I can see a welcome message
     Given I am on the homepage
     Then I should see a welcome message
+
+  Scenario: When not logged in I see an invite to login or register
+    Given I am not logged in
+    And I am on the homepage
+    Then I should see an invitation to login
+
+  Scenario: When I am logged in I see a link to add a new url
+    Given I am logged in
+    And I am on the homepage
+    Then I should see a link to add a new url
+
--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -5,3 +5,22 @@ end
 Then("I should see a welcome message") do
   expect(page).to have_content("Welcome")
 end
+
+Given('I am not logged in') do
+  register
+  logout
+end
+
+Then('I should see an invitation to login') do
+  expect(page).to have_content("Please login or register to create a new link")
+end
+
+Given('I am logged in') do
+  register
+  logout
+  login
+end
+
+Then('I should see a link to add a new url') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
